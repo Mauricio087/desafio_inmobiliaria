fbf30911fecf84f3aae08c308bf143953691429d
mauricio087 <m.river.cea@gmail.com>
2024-12-16 10:56:33 -0300

    ajustes en html

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,7 +1,7 @@
-const indexVn = [
+const propiedadesVn = [
   {
     nombre: "Apartamento de lujo en zona exclusiva",
-     src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
+    src: "https://fotos.perfil.com/2018/09/21/trim/950/534/nueva-york-09212018-366965.jpg",
     descripcion: "Apartamento ubicado en zona residencial de lujo.",
     ubicacion: "123 Luxury Lane, Prestige Suburb, CA 45678",
     habitaciones: 4,
@@ -9,9 +9,10 @@ const indexVn = [
     smoke: false,
     pets: false,
   },
+  // Otros 3 objetos...
 ];
 
-const indexAlq = [
+const propiedadesAlq = [
   {
     nombre: "Apartamento en el centro de la ciudad",
     src: "https://images.unsplash.com/photo-1522708323590-d24dbb6b0267?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8YXBhcnRtZW50fGVufDB8MHwwfHx8MA%3D%3D&auto=format&fit=crop&w=700&q=60",
@@ -22,4 +23,5 @@ const indexAlq = [
     smoke: false,
     pets: true,
   },
+  // Otros 3 objetos...
 ];
